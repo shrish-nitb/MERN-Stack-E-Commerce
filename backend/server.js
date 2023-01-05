abc50cd5e51a4505b1ff102bd81be0a976749c89
shrish-nitb <shrish108@gmail.com>
2023-01-05 16:00:18 +0530

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const products = require("./data/products");
+const app = express();
+const cors = require("cors");
+const fs = require("fs");
+const path = require("path");
+const publicPath = path.join(__dirname, '..',"frontend", "build");
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
+
+app.get("/api/pictures/:for", (req, res) => {
+  if (req.params.for === "slider-home") {
+    fs.readdir(path.join(__dirname, '..', "frontend", "public", "images", "slider-home"), (err, files) => {
+      res.json(files);
+    });
+  }
+});
+
+app.use(express.static(publicPath));
+app.get('*', (res, req) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+})
+app.listen(port, () => console.log(`App listening at port: ${port}`));
